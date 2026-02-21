f425040ddb4b45c7d63629ba0b964e53dd895d2e
razzat008 <93041325+razzat008@users.noreply.github.com>
2026-02-21 21:59:54 +0545

    key pairs

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,7 +1,31 @@
 #![allow(unused)]
-use ecdsa::{Signature as ECDSASignature, SigningKey, VerifyingKey, signature::Signer};
+use ecdsa::{Signature as ECDSASignature, SigningKey, VerifyingKey, signature::rand_core::OsRng};
 use k256::Secp256k1;
+use rand::{self, rngs::ThreadRng};
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Signature(ECDSASignature<Secp256k1>);
-pub struct PrivateKey(ECDSASignature<Secp256k1>);
-pub struct PublicKey(ECDSASignature<Secp256k1>);
+
+#[derive(Debug)]
+pub struct PrivateKey(SigningKey<Secp256k1>);
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PublicKey(VerifyingKey<Secp256k1>);
+
+impl PrivateKey {
+    pub fn new() -> Self {
+        PrivateKey(SigningKey::random(&mut OsRng))
+    }
+
+    pub fn public_key(&self) -> PublicKey {
+        // get the correspoing public key
+        PublicKey(*self.0.verifying_key())
+    }
+}
+
+impl Default for PrivateKey {
+    fn default() -> Self {
+        Self::new()
+    }
+}
